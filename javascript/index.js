30564ba9163f6c873c968da834c1f6d640f72f7b
Courtney McGill <courtneymcgill@Courtneys-MacBook-Air.local>
2018-11-26 23:22:04 -0500

    changed foramtting, still styling

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
 // Get the DOM object to change color.
-var x = document.getElementById("Name");
-
+//var x = document.getElementById("Name");
+const x = document.querySelector('#Name');
+console.log("x is:", x);
 // Chose our starting colors and cooresponding RBG values.
 var first_color = '#'+Math.floor(Math.random()*16777215).toString(16);
 var first_red = parseInt(first_color.slice(1, 3),16);
@@ -13,7 +14,7 @@ var second_red = parseInt(second_color.slice(1, 3),16);
 var second_blue = parseInt(second_color.slice(3, 5),16);
 var second_green = parseInt(second_color.slice(5, 7),16);
 
-console.log(first_color, first_red, first_blue, first_green);
+//console.log(first_color, first_red, first_blue, first_green);
 
 // Execute this code every 30miliseconds.
 setInterval(function() {
@@ -25,12 +26,15 @@ setInterval(function() {
       var second_blue = parseInt(second_color.slice(3, 5),16);
       var second_green = parseInt(second_color.slice(5, 7),16);
 
-      x.style.color = first_color
-      console.log("first_red, second_red:", first_red, second_red);
-      console.log("first_blue, second_blue:", first_blue, second_blue);
-      console.log("first_green, second_green:", first_green, second_green);
+ //     x.style.color = first_color
+      x.innerHTML = x.innerHTML.replace('M', '<span style="color:' + first_color + '">M</span>');
+      x.innerHTML = x.innerHTML.replace('L', '<span style="color:' + first_color + '">L</span>');
+ //     x.innerHTML = x.innerHTML.replace('M', '<span style="color: first_color;">o</span>');
+ //     console.log("first_red, second_red:", first_red, second_red);
+ //     console.log("first_blue, second_blue:", first_blue, second_blue);
+ //    console.log("first_green, second_green:", first_green, second_green);
       if (first_red == second_red && first_blue == second_blue && first_green == second_green){
-      console.log("colors match");
+ //     console.log("colors match");
       second_color = '#'+Math.floor(Math.random()*16777215).toString(16);
       second_red = parseInt(second_color.slice(1, 3),16);
       second_blue = parseInt(second_color.slice(3, 5),16);
@@ -38,8 +42,8 @@ setInterval(function() {
       }
 
       // check our starting and target colors
-      console.log("the first color is:", first_color, first_red, first_blue, first_green);
-      console.log("the second color is:",second_color, second_red, second_blue, second_green);
+ //     console.log("the first color is:", first_color, first_red, first_blue, first_green);
+ //     console.log("the second color is:",second_color, second_red, second_blue, second_green);
 
       // chose the color we want to manipulate
       var remaining_colors = []
@@ -52,42 +56,42 @@ setInterval(function() {
       if (first_green != second_green){
             remaining_colors.push("green");
       }
-      console.log("our remaining colrs are:",remaining_colors);
+//      console.log("our remaining colrs are:",remaining_colors);
       num_mismatch_colors = remaining_colors.length;
       random_choice = Math.floor(Math.random() * num_mismatch_colors);
-      console.log("our random choice was:",random_choice);
+//      console.log("our random choice was:",random_choice);
       chosen_color = remaining_colors[random_choice];
-      console.log("our chosen color was:",chosen_color);
+//      console.log("our chosen color was:",chosen_color);
 
       // Incrament or decrament the selected color depending on its relation to the target value for that color.
       if (chosen_color == 'red'){
             if (first_red < second_red){
                   first_red ++;
-                  console.log("incrementing red to ", first_red);
+ //                 console.log("incrementing red to ", first_red);
             }
             else{
                   first_red --;
-                  console.log("decrementing red to ", first_red);
+//                  console.log("decrementing red to ", first_red);
             }     
       }
       else if (chosen_color == 'blue'){
             if (first_blue < second_blue){
                   first_blue ++;
-                  console.log("incrementing blue to ", first_blue);
+//                  console.log("incrementing blue to ", first_blue);
             }
             else{
                   first_blue --;
-                  console.log("decrementing blue to ", first_blue);
+//                  console.log("decrementing blue to ", first_blue);
             } 
       }
       else if (chosen_color == 'green'){
             if (first_green < second_green){
                   first_green ++;
-                  console.log("incrementing green to ", first_green);
+//                  console.log("incrementing green to ", first_green);
             }
             else{
                   first_green --;
-                  console.log("decrementing green to ", first_green);
+//                  console.log("decrementing green to ", first_green);
             }     
       }
       console.log('revised are:', first_red, first_blue, first_green);
@@ -105,5 +109,5 @@ setInterval(function() {
       }
       var new_first_color_num = hex_red + hex_blue + hex_green
       first_color = '#'+new_first_color_num
-      console.log('our new first color is:', first_color);
+//      console.log('our new first color is:', first_color);
 }, 30) ;
